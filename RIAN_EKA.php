81b801856f7ea5109d8c79a36235b1a4ce2e2dba
unknown <2224043.rian@universitaspahlawan.ac.id>
2023-11-20 15:54:45 +0700

    rian_2255201043

--- RIAN_EKA.php
+++ RIAN_EKA.php
@@ -5,41 +5,43 @@ class mahasiswa
     private $name;
     private $nim;
     private $matkul;
+    private $citacita;
     // Methods
-    function set_name($name)
+    function __construct($name, $nim, $matkul, $citacita)
     {
         $this->name = $name;
+        $this->nim = $nim;
+        $this->matkul = $matkul;
+        $this->citacita = $citacita;
     }
     function get_name()
     {
         return $this->name;
     }
-    function set_nim($nim)
-    {
-        $this->nim = $nim;
-    }
+    
     function get_nim()
     {
         return $this->nim;
     }
-    function set_matkul($matkul)
-    {
-        $this->matkul = $matkul;
-    }
+    
     function get_matkul()
     {
         return $this->matkul;
     }
+    function get_citacita()
+    {
+        return $this->citacita;
+    }
 }
 
-$rian = new mahasiswa();
-$rian->set_name('RIAN EKA PUTRA');
-$rian->set_nim('2255201043');
-$rian->set_matkul('PBO');
+$rian = new mahasiswa('rian eka putra', '2255201043', 'PBO', 'menjadi proplayer');
+
 echo "Name: " . $rian->get_name();
 echo "\n";
 echo "nim: " . $rian->get_nim();
 echo "\n";
 echo "matkul: " . $rian->get_matkul();
+echo "\n";
+echo "citacita: " . $rian->get_citacita();
 ?>
  
