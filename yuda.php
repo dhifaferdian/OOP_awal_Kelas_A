43d1945fd209b129d132a530cc56226cea39e9d3
celloboxboy <123570574+celloboxboy@users.noreply.github.com>
2023-11-21 19:37:04 +0700

    yuda_2255201039
    yuda
    2255201039
    construct

--- yuda.php
+++ yuda.php
@@ -5,39 +5,42 @@ class mahasiswa
     private $name;
     private $nim;
     private $mkul;
+    private $citacita;
     // Methods
-    function set_name($name)
+    function __construct($name, $nim, $mkul, $citacita)
     {
-        $this->name = $name;
+        $this->name = $name; 
+        $this->nim = $nim; 
+        $this->mkul = $mkul; 
+        $this->citacita = $citacita; 
     }
+
+    
     function get_name()
     {
         return $this->name;
     }
-    function set_nim($nim)
-    {
-        $this->nim = $nim;
-    }
+
     function get_nim()
     {
         return $this->nim;
     }
-    function set_mkul($mkul)
-    {
-        $this->mkul = $mkul;
-    }
+
     function get_mkul()
     {
         return $this->mkul;
     }
+    function get_citacita()
+    {
+        return $this->citacita;
+    }
 }
 
-$yuda = new mahasiswa();
-$yuda->set_name('yuda');
-$yuda->set_nim('2255201039');
-$yuda->set_mkul('pbo');
+$yuda = new mahasiswa('yuda','2255201039','PBO','menjadi power ranger hijau');
 echo "Name: " . $yuda->get_name();
 echo "\n";
 echo "nim: " . $yuda->get_nim();
 echo "\n";
 echo "mkul: " . $yuda->get_mkul();
+echo "\n";
+echo "citacita: " . $yuda->get_citacita();
