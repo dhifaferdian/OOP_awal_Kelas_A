05070a5fec570edd40bfd57cce9ddda8e9b3a908
Jefrinaldi <150009001+Jefrinaldi@users.noreply.github.com>
2023-11-21 12:43:43 +0700

    Jefrinaldi.php

--- Jefrinaldi.php
+++ Jefrinaldi.php
@@ -1,43 +1,60 @@
+<!DOCTYPE html>
+<html>
+<body>
 <?php
-class mahasiswa
-{
-    // Properties
-    private $name;
-    private $nim;
-    private $matkul;
-    // Methods
-    function set_name($name)
-    {
-        $this->name = $name;
-    }
-    function get_name()
-    {
-        return $this->name;
-    }
-    function set_nim($nim)
-    {
-        $this->nim = $nim;
-    }
-    function get_nim()
-    {
-        return $this->nim;
-    }
-    function set_matkul($matkul)
-    {
-        $this->matkul = $matkul;
-    }
-    function get_matkul()
-    {
-        return $this->matkul;
-    }
+class mahasiswa {
+  // Properties
+  private $name;
+  private $nim;
+  private $matkul;
+  private $cita_cita;
+
+  // Methods
+  function set_name($name) {
+    $this->name = $name;
+  function __construct ($name, $nim, $matkul, $cita_cita){
+  	$this->name = $name;
+    $this->nim = $nim;
+    $this->matkul = $matkul;
+    $this->cita_cita = $cita_cita;
+  }
+  function get_name() {
+    return $this->name;
+  }
+  function set_nim($nim) {
+    $this->nim = $nim;
+  }
+  function get_nim() {
+    return $this->nim;
+  }
+  function set_matkul($matkul) {
+  	$this->matkul = $matkul;
+  }
+  function get_matkul () {
+  function get_matkul() {
+  	return $this->matkul;
+  }
+  function get_cita_cita() {
+  	return $this->cita_cita;
+  }
 }
 
-$Jeff = new mahasiswa();
-$Jeff->set_name('Jefrinaldi');
-$Jeff->set_nim('2255201021');
-$Jeff->set_matkul('pbo');
-echo "Name: " . $Jeff->get_name();
-echo "\n";
-echo "nim: " . $Jeff->get_nim();
-echo "\n";
-echo "matkul: " . $Jeff->get_matkul();
+$Jefri = new mahasiswa();
+$Jefri->set_name('Jefrinaldi');
+$Jefri->set_nim('2255201021');
+$Jefri->set_matkul('pbo');
+echo "Nama: " . $Jefri->get_name();
+$Jefri = new mahasiswa('Jefrinaldi', '2255201021', 'pbo', 'Hebaatt');
+echo $Jefri->get_name();
+echo "<br>";
+echo $Jefri->get_nim();
+echo "<br>";
+echo "nim: " .  $Jefri->get_nim();
+echo $Jefri->get_matkul();
+echo "<br>";
+echo "matkul: " . $Jefri->get_matkul();
+echo $Jefri->get_cita_cita();
+
+?>
+
+</body>
