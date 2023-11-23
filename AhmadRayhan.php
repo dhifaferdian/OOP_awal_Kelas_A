c01cf9a2d29aba5a61c0fb2d068bc24058a5ff98
UwoRayhan <148047183+UwoRayhan@users.noreply.github.com>
2023-11-23 10:39:13 +0700

    AhmadRayhan.php

--- AhmadRayhan.php
+++ AhmadRayhan.php
@@ -21,16 +21,9 @@ class mahasiswa {
   function get_name() {
     return $this->name;
   }
-  function set_nim($nim) {
-    $this->nim = $nim;
-  }
   function get_nim() {
     return $this->nim;
   }
-  function set_matkul($matkul) {
-  	$this->matkul = $matkul;
-  }
-  function get_matkul () {
   function get_matkul() {
   	return $this->matkul;
   }
@@ -39,22 +32,14 @@ class mahasiswa {
   }
 }
 
-$rehan = new mahasiswa();
-$rehan->set_name('Ahmad Rayhan');
-$rehan->set_nim('2255201053');
-$rehan->set_matkul('pbo');
-echo "Nama: " . $rehan->get_name();
-$Furqon = new mahasiswa('Ahmad Rayhan', '2255201053', 'pbo', 'Pembalap');
+$rehan = new mahasiswa('Ahmad Rayhan', '2255201053', 'pbo', 'Pembalap');
 echo $rehan->get_name();
 echo "<br>";
 echo $rehan->get_nim();
 echo "<br>";
-echo "nim: " .  $rehan->get_nim();
 echo $rehan->get_matkul();
 echo "<br>";
-echo "matkul: " . $rehan->get_matkul();
 echo $rehan->get_cita_cita();
-
 ?>
 
 </body>
